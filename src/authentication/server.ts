9076364d12af407eaa7d6431d9d5f7b416657603
[Vlad] <[vr.loudness@gmail.com]>
2018-12-07 00:18:12 -0500

    server works

--- src/authentication/server.ts
+++ src/authentication/server.ts
@@ -1,27 +1,55 @@
 import { Express } from 'express';
 import express from 'express';
 import OAuthServer from 'express-oauth-server';
+import { ClientCredentialsModel } from 'oauth2-server';
+import { AccessService } from './access.service';
+import bodyParser = require('body-parser');
+import { Server } from 'http';
 
-interface AuthenticationServer extends Express {
+export interface AuthenticationServer extends Express {
   oauth: any;
 }
 
 export interface AuthenticationApp {
-  app: AuthenticationServer;
   start(): void;
+  stop(): void;
+  getApp(): AuthenticationServer;
 }
 
 export class AuthenticationAppImpl implements AuthenticationApp {
-  app: AuthenticationServer;
-  constructor() {}
+  private app: AuthenticationServer;
+  private server: Server;
+  constructor() {
+    this.app = this.createApp();
+  }
   private createApp(): AuthenticationServer {
     const expressApp = express();
     const app = <AuthenticationServer>expressApp;
-    const oauth = new OAuthServer({
-      model: {}, // See https://github.com/oauthjs/node-oauth2-server for specification
+    const model: any = new AccessService();
+    app.oauth = new OAuthServer({
+      model, // See https://github.com/oauthjs/node-oauth2-server for specification
     });
-    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(app.oauth.authorize());
+
+    app.use(function(req, res) {
+      res.send('Secret area');
+    });
+
+    app.get('/authenticate', (req, res) => {
+      res.status(200);
+    });
+
     return app;
   }
-  start(): void {}
+  async start() {
+    this.server = await this.app.listen(3000);
+  }
+  async stop() {
+    await this.server.close();
+  }
+  getApp() {
+    return this.app;
+  }
 }
--- src/authentication/tests/integration/server.integration.ts
+++ src/authentication/tests/integration/server.integration.ts
@@ -4,6 +4,7 @@ import {
   AuthenticationApp,
   AuthenticationAppImpl,
 } from '../../server';
+import { expect } from 'chai';
 const SET_JSON = ['Accept', 'application/json'];
 
 let app: AuthenticationApp;
@@ -11,14 +12,17 @@ before(async () => {
   app = new AuthenticationAppImpl();
   await app.start();
 });
+after(async () => {
+  app.stop();
+});
 describe('Authentication server', () => {
   describe('core', () => {
     it('authenticates user', async () => {
-      const { body } = await supertest(app)
+      const { body } = await supertest(app.getApp())
         .get('/authenticate')
         .set(SET_JSON)
-        .expect(200);
       console.log(body);
+      expect(body).to.be.not.null;
     });
   });
 });
